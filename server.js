5aed92b5b9e775a25dcf3747fd86c15794fc38c6
p1p1ng <you@example.com>
2025-03-20 22:47:04 +0700

    Fixed merge conflicts and added latest changes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -16,3 +17,23 @@ app.use('/api/users', userRoutes);
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+=======
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Import Routes
+const userRoutes = require('./routes/users');
+app.use('/api/users', userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+>>>>>>> 7f79807 (tambahkan)
