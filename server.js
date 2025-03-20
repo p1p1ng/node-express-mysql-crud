0eca0fec1b6e260cefc7939f7f0f001a267acd3a
p1p1ng <you@example.com>
2025-03-20 22:53:39 +0700

    update

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -17,23 +16,3 @@ app.use('/api/users', userRoutes);
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-=======
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(bodyParser.json());
-
-// Import Routes
-const userRoutes = require('./routes/users');
-app.use('/api/users', userRoutes);
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
->>>>>>> 7f79807 (tambahkan)
